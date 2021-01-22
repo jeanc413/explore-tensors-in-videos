9d5d922f0f2a869dd46eb756f784cb8b11a07e5f
Fernández Herrera, Jean <s4jnfern@uni-trier.de>
2021-01-22 11:21:01 +0100

    Update Comparison.py to use clustering by KMeans

--- Scripts/Comparison.py
+++ Scripts/Comparison.py
@@ -7,39 +7,18 @@ Created on Sat Nov 21 14:19:40 2020
 # %% import packages
 import pickle
 import glob
+import kmeans
 import tensorly as tl
-from TuckerFunction import video_tensor_decomposer, SubTensor
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-
-# %% define Tensor subset class
-name_list = glob.glob('*')
-name_list = [n for n in name_list if "HandWash_" in n]
-Tensor_List = []
-for tensor in name_list:
-    Handler = open(tensor, "rb")
-    Tensor_List.append(pickle.load(Handler))
-
-n = len(Tensor_List)
-
-Comparisons = pd.DataFrame(columns=["Left", "Right", "Difference"], dtype=float)
-
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            Append = pd.Series([name_list[i], name_list[j], 0], index=Comparisons.columns)
-            Comparisons = Comparisons.append(Append, ignore_index=True)
-        else:
-            Append = pd.Series([name_list[i], name_list[j], tl.norm(Tensor_List[i].core - Tensor_List[j].core)],
-                               index=Comparisons.columns)
-            Comparisons = Comparisons.append(Append, ignore_index=True)
+from sklearn.metrics.cluster import normalized_mutual_info_score
 
 
+# %% Create formatting and pre-processing functions
 def labeling(number):
     number = int(number)
-    label = ""
     if number <= 5:
         label = "DarkSteelSink1"
     elif number <= 8:
@@ -60,7 +39,7 @@ def labeling(number):
 def reformat_string(data):
     temp = np.empty(len(data), dtype=object)
     for k in range(len(data)):
-        temp[k] = labeling(data[1][i])
+        temp[k] = labeling(data[1][k])
     data["Type"] = temp
     data["Step"] = data[3]
     for k in range(6):
@@ -68,65 +47,35 @@ def reformat_string(data):
     return data
 
 
-Left = reformat_string(Comparisons["Left"].str.split("_", expand=True))
-Right = reformat_string(Comparisons["Right"].str.split("_", expand=True))
+def standardize(tensor_list):
+    core_list = [tensor_list[i].core for i in range(len(tensor_list))]
+    mean = tl.mean(core_list, axis=0)
+    std = tl.sqrt(((core_list - mean)**2)/len(tensor_list)-1)
+    standard_score = (core_list - mean)/std
+    for i in range(len(tensor_list)):
+        tensor_list[i].core = standard_score
+    return tensor_list
 
-Comparisons["Left Type"] = Left["Type"]
-Comparisons["Left Step"] = Left["Step"]
-Comparisons["Right Type"] = Right["Type"]
-Comparisons["Right Step"] = Right["Step"]
+# %% WARNING Change current directory to decomposition folder
+# %% Load SubTensors objects
+name_list = glob.glob('*')
+name_list = [n for n in name_list if "HandWash_" in n]
+Tensor_List = []
+for tensor in name_list:
+    Handler = open(tensor, "rb")
+    Tensor_List.append(pickle.load(Handler))
 
-# %% View Results
+n = len(Tensor_List)
 
-# Histogram of difference
-plt.hist(Comparisons["Difference"], color='pink', ec='black')
-plt.title("Histogram of differences between videos")
-plt.show()
+# %% Contingency table
+Clusters = kmeans.KMeans(Tensor_List, k=6).predict()
+Table = pd.DataFrame()
+Table["Type"] = reformat_string(pd.Series(name_list).str.split("_", expand=True))["Type"]
+Table["Clusters"] = Clusters
+Contingency = pd.crosstab(Table["Clusters"], Table["Type"], margins=True)
+print("Normalized Mutual Information", normalized_mutual_info_score(Table["Clusters"], Table["Type"]))
+print("Contingency Table", Contingency, sep="\n")
 
-# Boxplot 
-Filter = (Comparisons["Difference"] != 0) & (Comparisons["Right Type"] == Comparisons["Left Type"])
-sns.boxplot(data=Comparisons[Filter], y="Right Type", x="Difference").set_title(
-    "Boxplot for difference between same type of sink")
-plt.show()
-
-# Full comparison matrix
-Comp_Matrix = pd.pivot_table(Comparisons, values='Difference', index='Left', columns='Right', aggfunc=np.mean)
-
-# Comparing all from Step 01
-Filter = (Comparisons["Left Step"] == '01') & (Comparisons["Right Step"] == '01')
-Comp_Step_01 = pd.pivot_table(Comparisons[Filter], values='Difference', index='Left', columns='Right', aggfunc=np.mean)
-
-# Comparing all from DarkSteelSink1
-Filter = (Comparisons["Left Type"] == 'DarkSteelSink1') & (Comparisons["Right Type"] == 'DarkSteelSink1')
-Comp_DarkSteelSink1 = pd.pivot_table(Comparisons[Filter], values='Difference', index='Left', columns='Right',
-                                     aggfunc=np.mean)
-
-# Comparing between steps
-Comp_Step = pd.pivot_table(Comparisons, values='Difference', index='Left Step', columns='Right Step', aggfunc=np.mean)
-
-# Comparing between Types
-Comp_Type = pd.pivot_table(Comparisons, values='Difference', index='Left Type', columns='Right Type', aggfunc=np.mean)
-
-# Heatmaps
-sns.heatmap(Comp_Matrix, linewidths=0.5, yticklabels=True, xticklabels=True)
-sns.heatmap(Comp_Type, linewidths=1)
-sns.heatmap(Comp_Step, linewidths=1)
-
-for step in ["01", "02", "04"]:
-    Filter = (Comparisons["Left Step"] == step) & (Comparisons["Right Step"] == step)
-    Comp_Step_Type = pd.pivot_table(Comparisons[Filter], values='Difference', index='Left Type', columns='Right Type',
-                                    aggfunc=np.mean)
-    sns.heatmap(Comp_Step_Type, linewidths=0.5).set(title="Type of sink average difference from step " + step)
-    plt.show()
-
-All_Types = pd.unique(Comparisons["Right Type"])
-
-for Type in All_Types:
-    Filter = (Comparisons["Left Type"] == Type) & (Comparisons["Right Type"] == Type)
-    Comp_Type_Step = pd.pivot_table(Comparisons[Filter], values='Difference', index='Left Step', columns='Right Step',
-                                    aggfunc=np.mean)
-    sns.heatmap(Comp_Type_Step, linewidths=0.5).set(title="Step average difference from " + Type)
-    plt.show()
 
 # %% Inverse engineering the decomposition
 print(Tensor_List[0])
