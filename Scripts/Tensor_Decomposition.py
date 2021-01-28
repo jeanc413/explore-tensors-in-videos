7d4a58864a7f9748af56848c5bfde28a38a5518f
Jean Fernandez <s4jnfern@uni-trier.de>
2021-01-28 16:12:18 +0100

    Adding new SubTensors (2,2,2,2) and reformat script

--- Scripts/Tensor_Decomposition.py
+++ Scripts/Tensor_Decomposition.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import cv2
 import numpy as np
 import tensorly as tl
@@ -9,10 +8,7 @@ from TuckerFunction import video_tensor_decomposer
 import pickle  # For file writing
 import glob  # For File searching
 
-
-
-
-
+# %% Chage into appropiate path contaning videos samples
 video_list = glob.glob('Videos/*.MOV')  # Reading all files with MOV extension
 video_list2 = glob.glob('Videos/*.mp4')
 
@@ -23,21 +19,31 @@ seed = np.random.RandomState(123)  # reproducible seed state
 i = 0  # First List Index
 if video_list:
     for video in video_list:
-        decomposition = video_tensor_decomposer(video_list[i],rank=(32,32,32), seed_state=seed)
-        pickle_out = open(video_list[i].replace('.MOV', '').replace('Videos/','Decompositions/'), "wb")
+        decomposition = video_tensor_decomposer(video_list[i],
+                                                tensor_length=50, 
+                                                rank=(2,2,2,2),
+                                                seed_state=seed)
+        pickle_out = open(video_list[i].replace('.MOV', '')
+                          .replace('Videos/','Decompositions/'), "wb")
         pickle.dump(decomposition, pickle_out)
         pickle_out.close()
-        video_list.append(video_list[i].replace('.MOV', '').replace('Videos/','Decompositions/'))
+        video_list.append(video_list[i].replace('.MOV', '')
+                          .replace('Videos/','Decompositions/'))
         i = i + 1
 
 j = 0    # Second list index
 if video_list2:
     for video in video_list2:
-        decomposition = video_tensor_decomposer(video_list2[j],rank=(32,32,32), seed_state=seed)
-        pickle_out = open(video_list2[j].replace('.mp4', '').replace('Videos/','Decompositions/'), "wb")  # We use Main index for naming
+        decomposition = video_tensor_decomposer(video_list2[j],
+                                                tensor_length=50,
+                                                rank=(2,2,2,2),
+                                                seed_state=seed)
+        pickle_out = open(video_list2[j].replace('.mp4', '')
+                          .replace('Videos/','Decompositions/'), "wb")  # We use Main index for naming
         pickle.dump(decomposition, pickle_out)
         pickle_out.close() 
-        video_list.append(video_list2[j].replace('.mp4', '').replace('Videos/','Decompositions/'))
+        video_list.append(video_list2[j].replace('.mp4', '')
+                          .replace('Videos/','Decompositions/'))
         j = j + 1
 
 # %% Saving List as an object
